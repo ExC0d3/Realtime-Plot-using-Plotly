6155aa5c11ba73c74b64060c93aa809443675c27
ExC0d3 <sharma.abhinav503@gmail.com>
2016-04-22 12:43:45 +0530

    Initial commit

---
+++ public/js/custom.js
@@ -0,0 +1,11 @@
+function test(){
+	var graph = $('#graph');
+
+	Plotly.plot(graph,[
+	{
+		x:[1,2,3,4,5],
+		y:[1,2,4,8,16]
+	}], {
+		margin:{t:0}
+	});
+}
---
+++ test.js
@@ -0,0 +1,5 @@
+setInterval(()=>{
+	var x = 1+Math.random();
+	var y = 1+Math.random();
+	console.log("X:",x,"Y:",y);
+},1000);
